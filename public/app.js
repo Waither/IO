5c22c9b810cec96a0ba0746c650ff42d64646f93
Maciej Gąsior <maciek550@interia.eu>
2025-06-04 21:33:58 +0200

    usuwanie komentarzy

--- public/app.js
+++ public/app.js
@@ -198,21 +198,3 @@ async function loadContent() {
         new Input(input);
     });
 }
-
-
-
-// 4. Widok kierowców
-// Można dodać na select do pobierania z selecta kierowcy i pokazywania przypisania
-
-// document.getElementById('form-driver-view').onsubmit = async e=>{
-//     e.preventDefault();
-//     const {driverId}=Object.fromEntries(new FormData(e.target));
-//     const list=await api('GET',`/api/queries/driver-assign?driverId=${driverId}`);
-//     const body=document.getElementById('driver-body');
-//     body.innerHTML='';
-//     list.forEach(a=>{
-//         const tr=document.createElement('tr');
-//         tr.innerHTML=`<td>${a.order_id}</td><td>${a.status}</td>`;
-//         body.appendChild(tr);
-//     });
-// };
